23bbb40df2e652edf91f38ad18dd17ea42551584
sarthack12 <sarthak@shubhamtanks.com>
2026-04-14 12:26:32 +0530

    favicon for all devices

--- serviceworker.js
+++ serviceworker.js
@@ -1,7 +1,12 @@
 const CACHE_NAME = "elk-landing-v1";
 
 const STATIC_ASSETS = [
-    "favicon.png"
+    "index.html",
+    "manifest.json",
+    "favicon.png",
+    "icon-192.png",
+    "icon-512.png",
+    "apple-touch-icon.png"
 ];
 
 self.addEventListener("install", event => {
@@ -17,6 +22,8 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
     event.respondWith(
-        fetch(event.request).catch(() => caches.match(event.request))
+        fetch(event.request)
+            .then(response => response)
+            .catch(() => caches.match(event.request).then(cached => cached || caches.match("index.html")))
     );
 });
